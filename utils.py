023336cd2c46494037bd7d99a491739eb0d66451
AlexSaturno <50325382+AlexSaturno@users.noreply.github.com>
2024-08-16 16:59:27 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 # ----------------------------------
 # PDF
@@ -150,14 +149,10 @@ def configurar_driver():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")  # Melhor para headless
     chrome_options.add_argument("--no-sandbox")  # Para evitar alguns problemas de sandbox em sistemas baseados em Linux
-
-    # Alterar a estratégia de carregamento da página
-    caps = DesiredCapabilities().CHROME
-    caps["pageLoadStrategy"] = "none"
     
     # service = Service(r"/usr/bin/chromedriver")
     service = Service(r"/usr/lib/chromium-browser/chromedriver")
-    driver = webdriver.Chrome(service=service, options=chrome_options, desired_capabilities=caps)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     # driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.set_page_load_timeout(2500) 
     return driver
